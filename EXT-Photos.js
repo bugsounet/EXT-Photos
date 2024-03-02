edca0fea7165e30683b5b3c24b7fbd28a22c0711
bugsounet <bugsounet@bugsounet.fr>
2024-03-02 23:45:43 +0100

    eslint fix

--- EXT-Photos.js
+++ EXT-Photos.js
@@ -5,7 +5,7 @@
  ** support: https://forum.bugsounet.fr
  **/
 
-logPhotos = (...args) => { /* do nothing */ }
+logPhotos = (...args) => { /* do nothing */ };
 
 Module.register("EXT-Photos", {
   defaults: {
@@ -14,73 +14,73 @@ Module.register("EXT-Photos", {
     loop: false
   },
 
-  start: function () {
-    if (this.config.debug) logPhotos = (...args) => { console.log("[PHOTOS]", ...args) }
+  start () {
+    if (this.config.debug) logPhotos = (...args) => { console.log("[PHOTOS]", ...args); };
     this.photos= {
       position: 0,
       urls: [],
       length: 0,
       running: false
-    }
-    this.timerPhoto = null
-    this.ready = false
+    };
+    this.timerPhoto = null;
+    this.ready = false;
   },
 
-  getDom: function() {
-    var dom = document.createElement("div")
-    dom.style.display = 'none'
-    return dom
+  getDom () {
+    var dom = document.createElement("div");
+    dom.style.display = "none";
+    return dom;
   },
 
-  getStyles: function () {
-    return [ "EXT-Photos.css" ]
+  getStyles () {
+    return [ "EXT-Photos.css" ];
   },
 
-  getTranslations: function() {
+  getTranslations () {
     return {
       en: "translations/en.json",
       fr: "translations/fr.json"
-    }
+    };
   },
 
-  notificationReceived: function(noti, payload, sender) {
-    if (noti == "GA_READY") {
-      if (sender.name == "MMM-GoogleAssistant") {
-        this.sendSocketNotification("INIT")
-        this.preparePopup()
-        this.ready = true
-        this.sendNotification("EXT_HELLO", this.name)
+  notificationReceived (noti, payload, sender) {
+    if (noti === "GA_READY") {
+      if (sender.name === "MMM-GoogleAssistant") {
+        this.sendSocketNotification("INIT");
+        this.preparePopup();
+        this.ready = true;
+        this.sendNotification("EXT_HELLO", this.name);
       }
     }
-    if (!this.ready) return
+    if (!this.ready) return;
 
     switch(noti) {
       case "EXT_PHOTOS-OPEN":
-        logPhotos("Received:", payload)
+        logPhotos("Received:", payload);
         if (!payload || !payload.length) {
           this.sendNotification("EXT_ALERT", {
             message: this.translate("PhotosError"),
             type: "error"
-          })
-          return
+          });
+          return;
         }
-        this.photos.urls = payload
-        this.photos.position = 0
-        this.photos.length = payload.length
-        this.startPhotos()
-        break
+        this.photos.urls = payload;
+        this.photos.position = 0;
+        this.photos.length = payload.length;
+        this.startPhotos();
+        break;
       case "EXT_STOP":
         if (this.photos.running) {
-          this.resetPhotos()
-          this.endPhotos()
+          this.resetPhotos();
+          this.endPhotos();
         }
-        break
+        break;
       case "EXT_PHOTOS-CLOSE":
         if (this.photos.running) {
-          this.resetPhotos()
-          this.endPhotos(true)
+          this.resetPhotos();
+          this.endPhotos(true);
         }
-        break
+        break;
     }
   },
 
@@ -88,128 +88,128 @@ Module.register("EXT-Photos", {
   /** Popup Displayer **/
   /*********************/
 
-  preparePopup: function() {
-    var Photos = document.createElement("div")
-    Photos.id = "EXT_PHOTOS"
-    Photos.classList.add("hidden")
-    var backPhoto = document.createElement("div")
-    backPhoto.id = "EXT_PHOTOS_BACK"
-    var currentPhoto = document.createElement("div")
-    currentPhoto.id = "EXT_PHOTOS_CURRENT"
-    currentPhoto.addEventListener('animationend', ()=>{
-      currentPhoto.classList.remove("animated")
-    })
-    Photos.appendChild(backPhoto)
-    Photos.appendChild(currentPhoto)
-    document.body.appendChild(Photos)
+  preparePopup () {
+    var Photos = document.createElement("div");
+    Photos.id = "EXT_PHOTOS";
+    Photos.classList.add("hidden");
+    var backPhoto = document.createElement("div");
+    backPhoto.id = "EXT_PHOTOS_BACK";
+    var currentPhoto = document.createElement("div");
+    currentPhoto.id = "EXT_PHOTOS_CURRENT";
+    currentPhoto.addEventListener("animationend", ()=>{
+      currentPhoto.classList.remove("animated");
+    });
+    Photos.appendChild(backPhoto);
+    Photos.appendChild(currentPhoto);
+    document.body.appendChild(Photos);
   },
 
-  startPhotos: function() {
+  startPhotos () {
     if (!this.photos.running) this.sendNotification("EXT_ALERT", {
       message: this.translate("PhotosOpen"),
       type: "information",
       icon: "modules/EXT-Photos/resources/Photos-Logo.png"
-    })
-    this.sendNotification("EXT_PHOTOS-CONNECTED")
-    this.hideModules()
-    this.showPhotos()
-    this.photoDisplay()
+    });
+    this.sendNotification("EXT_PHOTOS-CONNECTED");
+    this.hideModules();
+    this.showPhotos();
+    this.photoDisplay();
   },
 
-  endPhotos: function(extAlert=false) {
+  endPhotos (extAlert=false) {
     if (extAlert) this.sendNotification("EXT_ALERT", {
       message: this.translate("PhotosClose"),
       type: "information",
       icon: "modules/EXT-Photos/resources/Photos-Logo.png"
-    })
-    this.sendNotification("EXT_PHOTOS-DISCONNECTED")
-    this.hidePhotos()
-    this.showModules()
+    });
+    this.sendNotification("EXT_PHOTOS-DISCONNECTED");
+    this.hidePhotos();
+    this.showModules();
   },
 
-  showPhotos: function () {
-    logPhotos("Show Photos Iframe")
-    var iframe = document.getElementById("EXT_PHOTOS")
-    iframe.classList.remove("hidden")
+  showPhotos () {
+    logPhotos("Show Photos Iframe");
+    var iframe = document.getElementById("EXT_PHOTOS");
+    iframe.classList.remove("hidden");
   },
 
-  hidePhotos: function () {
-    logPhotos("Hide Photos Iframe")
-    var iframe = document.getElementById("EXT_PHOTOS")
-    iframe.classList.add("hidden")
+  hidePhotos () {
+    logPhotos("Hide Photos Iframe");
+    var iframe = document.getElementById("EXT_PHOTOS");
+    iframe.classList.add("hidden");
   },
 
-  hideModules: function () {
+  hideModules () {
     MM.getModules().enumerate((module)=> {
-      module.hide(100, {lockString: "EXT_PHOTOS-LOCKED"})
-    })
+      module.hide(100, { lockString: "EXT_PHOTOS-LOCKED" });
+    });
   },
 
-  showModules: function () {
+  showModules () {
     MM.getModules().enumerate((module)=> {
-      module.show(100, {lockString: "EXT_PHOTOS-LOCKED"})
-    })
+      module.show(100, { lockString: "EXT_PHOTOS-LOCKED" });
+    });
   },
 
-/** photos code **/
-  photoDisplay: function () {
-    this.photos.running = true
-    logPhotos("Loading photo #" + (this.photos.position) + "/" + (this.photos.length-1))
-    var hidden = document.createElement("img")
+  /** photos code **/
+  photoDisplay () {
+    this.photos.running = true;
+    logPhotos(`Loading photo #${this.photos.position}/${this.photos.length-1}`);
+    var hidden = document.createElement("img");
     hidden.onerror = () => {
       this.sendNotification("EXT_ALERT", {
         message: this.translate("PhotosError"),
         type: "warning",
         icon: "modules/EXT-Photos/resources/Photos-Logo.png"
-      })
-      logPhotos("Photo Loading Error... retry with next")
-      clearTimeout(this.timerPhoto)
-      this.timerPhoto = null
-      this.photoNext()
-    }
+      });
+      logPhotos("Photo Loading Error... retry with next");
+      clearTimeout(this.timerPhoto);
+      this.timerPhoto = null;
+      this.photoNext();
+    };
     hidden.onload = () => {
-      var back = document.getElementById("EXT_PHOTOS_BACK")
-      var current = document.getElementById("EXT_PHOTOS_CURRENT")
-      var dom = document.getElementById("EXT_PHOTOS")
-      logPhotos("URL=", this.photos.urls[this.photos.position])
-      back.style.backgroundImage = `url(${this.photos.urls[this.photos.position]})`
-      current.style.backgroundImage = `url(${this.photos.urls[this.photos.position]})`
-      current.classList.add("animated")
+      var back = document.getElementById("EXT_PHOTOS_BACK");
+      var current = document.getElementById("EXT_PHOTOS_CURRENT");
+      var dom = document.getElementById("EXT_PHOTOS");
+      logPhotos("URL=", this.photos.urls[this.photos.position]);
+      back.style.backgroundImage = `url(${this.photos.urls[this.photos.position]})`;
+      current.style.backgroundImage = `url(${this.photos.urls[this.photos.position]})`;
+      current.classList.add("animated");
       this.timerPhoto = setTimeout( () => {
-        this.photoNext()
-      }, this.config.displayDelay)
-    }
-    hidden.src = this.photos.urls[this.photos.position]
+        this.photoNext();
+      }, this.config.displayDelay);
+    };
+    hidden.src = this.photos.urls[this.photos.position];
   },
 
-  photoNext: function () {
+  photoNext () {
     if (this.photos.position >= (this.photos.length-1)) {
       if (this.config.loop) {
-        this.photos.position = 0
-        this.photoDisplay()
+        this.photos.position = 0;
+        this.photoDisplay();
       } else {
-        this.resetPhotos()
-        this.endPhotos(true)
+        this.resetPhotos();
+        this.endPhotos(true);
       }
     } else {
-      this.photos.position++
-      this.photoDisplay()
+      this.photos.position++;
+      this.photoDisplay();
     }
   },
 
-  resetPhotos: function () {
-    var back = document.getElementById("EXT_PHOTOS_BACK")
-    var current = document.getElementById("EXT_PHOTOS_CURRENT")
-    clearTimeout(this.timerPhoto)
-    this.timerPhoto = null
+  resetPhotos () {
+    var back = document.getElementById("EXT_PHOTOS_BACK");
+    var current = document.getElementById("EXT_PHOTOS_CURRENT");
+    clearTimeout(this.timerPhoto);
+    this.timerPhoto = null;
     this.photos= {
       position: 0,
       urls: [],
       length: 0,
       running: false
-    }
-    back.removeAttribute("style")
-    current.removeAttribute("style")
-    logPhotos("Reset Photos", this.photos)
+    };
+    back.removeAttribute("style");
+    current.removeAttribute("style");
+    logPhotos("Reset Photos", this.photos);
   }
-})
+});
